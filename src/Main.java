4e19d58b017c7c81d9aba55a8e6b885ec12df455
Dayton Shor <dayton.shor@gmain.com>
2017-11-10 10:43:50 -0500

    531234

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,13 @@ import java.io.FileOutputStream;
 public class Main {
     public static long numSpaces = 0;
     public static DataOutputStream dataOutputStream;
-    public static int count = 0;
     public static void main(String[] args) throws Exception{
-        File root = new File("/");
+        File root = new File("/Users/Student/Desktop");
         File output = new File("/Users/Student/IdeaProjects/CISC3150Homework8/src/dir_tree.txt");
         output.createNewFile();
         dataOutputStream = new DataOutputStream(new FileOutputStream(output));
         File[] tree = root.listFiles();
         writeTree(tree);
-        System.out.println(count);
     }
     public static void writeTree(File[] tree)throws Exception{
         File[] subTree;
